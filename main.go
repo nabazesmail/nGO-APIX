711a7fb2ae229ae24a8b28af4a9b83a605d1dab1
Nabaz esmail <nabazesmail567@gmail.com>
2023-07-19 23:24:44 +0300

    setting up router!

--- main.go
+++ main.go
@@ -2,9 +2,8 @@
 package main
 
 import (
-	"github.com/gin-gonic/gin"
-	"github.com/nabazesmail/gopher/controllers"
 	"github.com/nabazesmail/gopher/initializers"
+	"github.com/nabazesmail/gopher/router"
 )
 
 func init() {
@@ -13,10 +12,6 @@ func init() {
 }
 
 func main() {
-	r := gin.Default()
-
-	// Create a route to create a new post
-	r.POST("/users", controllers.CreateUser)
-
+	r := router.SetupRouter()
 	r.Run()
 }
---
+++ router/routes.go
@@ -0,0 +1,16 @@
+// router/router.go
+package router
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/nabazesmail/gopher/controllers"
+)
+
+func SetupRouter() *gin.Engine {
+	r := gin.Default()
+
+	// Create a route to create a new user
+	r.POST("/users", controllers.CreateUser)
+
+	return r
+}
